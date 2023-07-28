46f6ec64f3874e32aa7a9101884e9cb74de8143f
Louis Beaumont <louis.beaumont@gmail.com>
2023-07-28 15:42:16 -0700

    feat: tags on the chart

--- app/dashboard/NeurosityFocusChart.tsx
+++ app/dashboard/NeurosityFocusChart.tsx
@@ -109,6 +109,7 @@ export const NeurosityFocusChart = ({ session, defaultStates, getStates, getTags
                     height={20}
                 />
             </Button>
+            {/* @ts-expect-error */}
             <Plot
                 // @ts-ignore
                 data={data}
