e61c584571aba7cfbc803789c55be282b8f9045d
Louis Beaumont <louis.beaumont@gmail.com>
2023-08-07 11:59:50 -0700

    improve status

--- app/dashboard/NeurosityStatus.tsx
+++ app/dashboard/NeurosityStatus.tsx
@@ -7,11 +7,34 @@ import {
     HoverCardContent,
     HoverCardTrigger,
 } from "@/components/ui/hover-card"
-import { QuestionMarkCircleIcon } from '@heroicons/react/20/solid';
+import { ArrowPathIcon, QuestionMarkCircleIcon } from '@heroicons/react/20/solid';
+import { Button } from '@/components/ui/button';
+import toast, { Toaster } from 'react-hot-toast';
 
-const NeurosityStatus = () => {
+interface Props {
+    userId: string
+}
+
+const NeurosityStatus = ({ userId }: Props) => {
     const [active, setActive] = useState(false);
     const supabase = createClientComponentClient()
+
+    const forceListen = async () => {
+        // simply tweak a token row will trigger a change
+        const { error } = await supabase
+            .from('tokens')
+            .update({ status: { valid: true, updated_at: new Date().toISOString() } })
+            .eq('user_id', userId)
+
+        if (error) {
+            console.error('Error fetching data:', error);
+            return;
+        }
+
+        toast.loading('Force-checking if your Neurosity is connected ...', {
+            duration: 2000,
+        })
+    }
 
     useEffect(() => {
 
@@ -41,12 +64,13 @@ const NeurosityStatus = () => {
 
     return (
         <div className="relative flex flex-col justify-center items-center gap-2 p-4 bg-white rounded-lg shadow-lg">
+            <Toaster />
             <div className={`w-4 h-4 rounded-full ${active ? 'bg-green-500' : 'bg-gray-500'} animate-pulse`}></div>
             <p className="text-sm text-gray-500 max-w-xs text-center">
                 {
                     active ?
                         'Receiving your brain activity ...' :
-                        'No activity has been received from your Neurosity, please power it, wear it, and make sure your Neurosity account is connected in the account tab'
+                        'No activity has been received from your Neurosity, please power it, wear it, and make sure your Neurosity account is connected in the account tab. It can take up to 5 minutes to show up here.'
                 }
             </p>
             {/* top right in parent */}
@@ -60,6 +84,10 @@ const NeurosityStatus = () => {
                     </HoverCardContent>
                 </HoverCard>
             </div>
+            {/* top left in parent */}
+            <ArrowPathIcon width={16}
+                className="absolute top-0 left-0 mt-2 ml-2 text-gray-500 transform rotate-180 hover:cursor-pointer"
+                onClick={forceListen} />
         </div>
     );
 };
