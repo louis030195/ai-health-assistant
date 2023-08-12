c4f79c3b5d3572f6f546204e17c26f6df6894dff
Louis Beaumont <louis.beaumont@gmail.com>
2023-08-12 14:47:57 +0200

    email in stipe link

--- app/onboarding/neurosity/page.tsx
+++ app/onboarding/neurosity/page.tsx
@@ -7,14 +7,15 @@ import NeurosityConnect from "@/components/NeurosityConnect";
 
 export default async function Onboarding() {
     const session = await getSession()
-    
+
     if (!session) {
         return redirect('/signin');
     }
     return (
         // center stuff vertically and horizontally
-        <div className="flex flex-col items-center justify-center h-screen gap-4">
+        <div className="flex flex-col items-center mt-20 h-screen gap-4">
             <NeurosityConnect session={session} onboarding={true} />
+            <GoToButton text="Next" path="/dashboard" />
         </div>
     )
 }
