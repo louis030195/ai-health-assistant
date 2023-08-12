46ee620d300b8dae377f48a1420b789e196bbc90
Louis Beaumont <louis.beaumont@gmail.com>
2023-08-12 09:51:09 +0200

    last 5 days

--- app/pricing/PricingPlan.tsx
+++ app/pricing/PricingPlan.tsx
@@ -179,7 +179,7 @@ export default function Example() {
                                 </div>
 
                                 <a
-                                    href="/signin"
+                                    href="https://buy.stripe.com/4gw7tb31Q6ZIckoaEE"
                                     aria-describedby="tier-biohacker"
                                     className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
