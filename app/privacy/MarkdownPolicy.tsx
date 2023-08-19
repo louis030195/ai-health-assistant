8407e317cca1de30cffd4729c5f80655fb008514
Louis Beaumont <louis.beaumont@gmail.com>
2023-08-19 10:04:52 -0700

    update privacy

--- app/privacy/MarkdownPolicy.tsx
+++ app/privacy/MarkdownPolicy.tsx
@@ -1,67 +1,118 @@
 'use client'
-import Markdown from '@/components/ui/Markdown';
 import React from 'react';
 
 const MarkdownPolicy = () => {
-    return (
-        <div className="mt-20 prose prose-lg">
-            <h1>Privacy Policy</h1>
+  return (
+    <div className="mt-20 prose prose-lg">
+      <h1>Privacy Policy</h1>
 
-            <p>At Mediar, we take privacy very seriously. This policy outlines how we collect, use, disclose, and protect your personal information.</p>
+      <p>
+        At Mediar, we take privacy very seriously. This policy outlines how we 
+        collect, use, disclose, and protect your personal information.
+      </p>
 
-            <h2>What Information We Collect</h2>
+      <h2>What Information We Collect</h2>
 
-            <p>We collect the following types of information about you:</p>
+      <p>We collect the following types of information about you:</p>
 
-            <ul>
-                <li><strong>Account information</strong> such as your name, email address, and password when you register for an account.</li>
-                <li><strong>Brain activity data</strong> such as EEG signals, frequencies, amplitudes, etc. collected from your wearable device.</li>
-                <li><strong>Health information</strong> such as medical history, conditions, medications, etc. that you provide to personalize your insights.</li>
-                <li><strong>Usage information</strong> such as how you interact with our services and website. This is collected through cookies, log files, and tracking tools.</li>
-            </ul>
+      <ul>
+        <li>
+          <strong>Account information</strong> such as your name, email address, 
+          and password when you register for an account.
+        </li>
+        <li>
+          <strong>Health information</strong> such as such as EEG signals, heart, sleep,
+          and physical activity. That allow us to provide you personalized insights. 
+        </li>
+        <li>
+          <strong>Usage information</strong> such as how you interact with our 
+          services and website. This is collected through cookies, log files, 
+          and tracking tools.
+        </li>
+      </ul>
 
-            <h2>How We Use Information</h2>
+      <h2>How We Use Information</h2>
 
-            <p>We use the information we collect for the following purposes:</p>
+      <p>
+        We use the information we collect for the following purposes:
+      </p>
 
-            <ul>
-                <li>Provide our services to you, which includes analyzing your brain data to generate personalized insights.</li>
-                <li>Improve and develop our products, including our AI algorithms.</li>
-                <li>Personalize content and experiences for you on our platform.</li>
-                <li>Communicate with you, such as through email, notifications, or ads.</li>
-                <li>Conduct research to advance our understanding of neuroscience, cognition, and brain health.</li>
-                <li>Comply with legal obligations for data processing, security, etc.</li>
-            </ul>
+      <ul>
+        <li>
+          Provide our services to you, which includes analyzing your health data 
+          to generate personalized insights.
+        </li>
+        <li>
+          Improve and develop our products, including our AI algorithms.
+        </li>
+        <li>
+          Personalize content and experiences for you on our platform. 
+        </li>
+        <li>
+          Communicate with you, such as through email, notifications, or ads.
+        </li>
+        <li>
+          Conduct research to advance our understanding of mind and body health.
+        </li>
+        <li>
+          Comply with legal obligations for data processing, security, etc.
+        </li>
+      </ul>
 
-            <h2>How We Share Information</h2>
+      <h2>How We Share Information</h2>
 
-            <p>We may share your information with the following parties:</p>
+      <p>
+        We may share your information with the following parties:
+      </p>
 
-            <ul>
-                <li>Service providers under contract who help operate our business, such as cloud hosting and AI providers.</li>
-            </ul>
+      <ul>
+        <li>
+          Service providers under contract who help operate our business, such as 
+          cloud hosting and AI providers.
+        </li>
+      </ul>
 
-            <h2>Your Data Rights</h2>
+      <h2>Your Data Rights</h2>
 
-            <p>As a user, you have the right to access, correct, delete, restrict, or object to our use of your personal data. Please contact us if you wish to exercise these rights.</p>
+      <p>
+        As a user, you have the right to access, correct, delete, restrict, or 
+        object to our use of your personal data. Please contact us if you wish 
+        to exercise these rights.
+      </p>
 
-            <h2>Data Security</h2>
+      <h2>Data Security</h2>
 
-            <p>We utilize encryption, access controls, data minimization, and other safeguards to protect your personal information. However, no data transmission over the internet is 100% secure, so we cannot guarantee security.</p>
+      <p>
+        We utilize encryption, access controls, data minimization, and other 
+        safeguards to protect your personal information. However, no data 
+        transmission over the internet is 100% secure, so we cannot guarantee 
+        security.
+      </p>
 
-            <h2>Data Retention</h2>
+      <h2>Data Retention</h2>
 
-            <p>We retain your personal information for as long as you have an account plus a reasonable period afterward in case you reactivate services. Brain activity data may be retained longer for research purposes after removing identifiers.</p>
+      <p>
+        We retain your personal information for as long as you have an account 
+        plus a reasonable period afterward in case you reactivate services. Health data may be retained longer for research purposes after removing 
+        identifiers.
+      </p>
 
-            <h2>Updates to this Policy</h2>
+      <h2>Updates to this Policy</h2>
 
-            <p>We may occasionally update this policy as our services evolve. You should revisit this page periodically to stay aware of any changes.</p>
+      <p>
+        We may occasionally update this policy as our services evolve. You should  
+        revisit this page periodically to stay aware of any changes.
+      </p>
 
-            <h2>Contact Us</h2>
+      <h2>Contact Us</h2>
 
-            <p>If you have any questions about this privacy policy or use of your personal data, please email us at louis@mediar.ai.</p>
-        </div>
-    );
+      <p>
+        If you have any questions about this privacy policy or use of your 
+        personal data, please email us at louis@mediar.ai.
+      </p>
+
+    </div>
+  );
 };
 
 export default MarkdownPolicy;
