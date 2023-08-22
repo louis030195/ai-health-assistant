23a3c68fdf500a59ca7fbcb661329eb54ab77e62
Louis Beaumont <louis.beaumont@gmail.com>
2023-08-22 15:40:55 -0700

    fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,6 +6,8 @@ import type { NextRequest } from 'next/server'
 import type { Database } from '@/types_db'
 import { getURL } from '@/utils/helpers'
 
+
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
@@ -13,7 +15,18 @@ export async function GET(request: NextRequest) {
   console.log('yo', requestUrl.origin)
   if (code) {
     const supabase = createRouteHandlerClient<Database>({ cookies })
-    await supabase.auth.exchangeCodeForSession(code)
+    const response = await supabase.auth.exchangeCodeForSession(code)
+
+    const { data, error } = await supabase
+      .from('onboardings')
+      .select('*')
+      .eq('user_id', response.data.user?.id)
+      .limit(1)
+    if (error) {
+      console.log(error.message);
+    }
+
+    if (!data) return NextResponse.redirect(`${getURL()}/onboarding/intro`)
   }
 
   // URL to redirect to after sign in process completes
