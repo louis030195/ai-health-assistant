7b25d5f5f6876cbc62a40d5b5f1186e8f77e650f
Louis Beaumont <louis.beaumont@gmail.com>
2023-08-24 17:19:59 -0700

    update landing

--- components/ui/WhatsappConnect.tsx
+++ components/ui/WhatsappConnect.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { useEffect, useState } from 'react';
 import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Button } from './button';
@@ -7,11 +8,10 @@ import { Input } from './input';
 import toast, { Toaster } from 'react-hot-toast';
 import { Database } from '@/types_db';
 import { VerificationData, VerificationResponse } from '@/app/whatsapp-server';
+import OtpInput from 'react-otp-input';
+import PhoneInput from 'react-phone-number-input';
 
-import 'react-phone-number-input/style.css'
-import PhoneInput from 'react-phone-number-input'
-import { getURL } from '@/utils/helpers';
-
+import 'react-phone-number-input/style.css';
 
 type Subscription = Database['public']['Tables']['subscriptions']['Row'];
 type UserDetails = Database['public']['Tables']['users']['Row'];
@@ -23,16 +23,37 @@ interface Prop {
     subscription?: Subscription;
     userDetails?: UserDetails;
 }
+
 export default function WhatsappConnect({ session, subscription, userDetails, startVerification, verifyOtp }: Prop) {
     const supabase = createClientComponentClient();
-
     const [loading, setLoading] = useState(false);
     const [phoneNumber, setPhoneNumber] = useState(userDetails?.phone || '');
     const [phoneNumberError, setPhoneNumberError] = useState('');
-    console.log('phoneNumber', phoneNumber);
+    const [otp, setOtp] = useState('');
+    const [showOtpInput, setShowOtpInput] = useState(false);
+
     useEffect(() => {
         validatePhoneNumber(phoneNumber);
     }, [phoneNumber]);
+
+    useEffect(() => {
+        if (otp.length === 6) {
+            const verifyOTP = async () => {
+                toast.loading('Verifying OTP...');
+                try {
+                    const response = await verifyOtp(phoneNumber, otp);
+                    if (response.status !== 'approved') throw new Error('Invalid OTP:' + response);
+                    setShowOtpInput(false);
+                } catch (error: any) {
+                    console.error(error);
+                    toast.error('Error verifying OTP. Please try again.');
+                }
+            }
+
+            verifyOTP();
+        }
+    }, [otp]);
+
     const validatePhoneNumber = (number: string) => {
         const regex = /^\+[1-9]\d{1,14}$/;
         if (!regex.test(number)) {
@@ -42,6 +63,7 @@ export default function WhatsappConnect({ session, subscription, userDetails, st
         setPhoneNumberError('');
         return true;
     }
+
     const handleConnect = async () => {
         if (!validatePhoneNumber(phoneNumber)) {
             return;
@@ -59,38 +81,7 @@ export default function WhatsappConnect({ session, subscription, userDetails, st
 
         try {
             await startVerification(phoneNumber);
-
-            const otp = prompt('Enter the OTP you received on WhatsApp');
-
-            toast.loading('Verifying OTP...', { id: toastId });
-            const response = await verifyOtp(phoneNumber, otp!);
-
-            if (response.status !== 'approved') throw new Error('Invalid OTP:' + response);
-
-            // fetch /api/phone-verified
-            const response2 = await fetch(`${getURL()}/api/phone-verified`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    userId: session.user?.id,
-                    phone: phoneNumber,
-                    full_name: userDetails?.full_name,
-                })
-            }).then(res => res.json());
-
-            console.log('response2', response2);
-
-            const { error } = await supabase.from('users').update({
-                phone: phoneNumber,
-                phone_verified: true
-            }).eq('id', session.user?.id);
-
-            if (error) throw error;
-
-            toast.success('WhatsApp connected successfully!', { id: toastId });
-            toast.success('Thank you for trying the beta integration of WhatsApp. Any issues, please contact us 🙏')
+            setShowOtpInput(true);
 
         } catch (error: any) {
             console.error(error);
@@ -109,14 +100,7 @@ export default function WhatsappConnect({ session, subscription, userDetails, st
             <p className="text-gray-500 mb-4">
                 Connect your WhatsApp account to receive insights and send tags directly on WhatsApp.
             </p>
-            {/* Phone number input */}
-            {/* <Input
-                type="text"
-                placeholder="+12345678901"
-                className="mb-4 text-black"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-            /> */}
+
             <PhoneInput
                 placeholder="Enter phone number"
                 value={phoneNumber}
@@ -125,6 +109,18 @@ export default function WhatsappConnect({ session, subscription, userDetails, st
             />
 
             {phoneNumberError && <p className="text-red-500 my-2">{phoneNumberError}</p>}
+
+            {
+                showOtpInput && (
+                    <OtpInput
+                        value={otp}
+                        onChange={setOtp}
+                        numInputs={6}
+                        renderInput={(props) => <input {...props} />}
+                        // className="mb-4"
+                    />
+                )
+            }
 
             <Button
                 onClick={handleConnect}
@@ -137,12 +133,9 @@ export default function WhatsappConnect({ session, subscription, userDetails, st
                 Send me a WhatsApp verification code
             </Button>
 
-            {/* small text saying that you should receive a whatsapp message from Mediar AI upon first time adding your phone number */}
             <p className="text-gray-500 mt-4">
                 You will receive a WhatsApp message from "Verify" to confirm your number. <br></br>After verifying, you will receive a welcome message from Mediar AI. Any issues, <a href="mailto:louis@mediar.ai" className="text-blue-500 underline">please contact us 🙏</a>.
             </p>
-
         </div>
     );
-
 }
