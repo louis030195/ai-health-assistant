068419ecbb719e6aae49764ed29a9bdd76d976f4
Louis Beaumont <louis.beaumont@gmail.com>
2023-08-26 09:54:36 -0700

    .

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -30,12 +30,12 @@ export default async function Dashboard() {
   if (!session) {
     return redirect('/signin');
   }
+  await saveOnboarding(session.user.id);
 
   const hasOnboarded = await getOnboarding(session.user.id);
   if (!hasOnboarded) {
     return redirect('/onboarding/intro');
   }
-  await saveOnboarding(session.user.id);
   const userDetails = await getUserDetails();
 
   const saveUserTimezoneServer = async (userId: string, timezone: string) => {
