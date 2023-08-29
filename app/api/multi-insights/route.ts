c866bbcbaf59305d6a0277d0494490bb4680eedc
Louis Beaumont <louis.beaumont@gmail.com>
2023-08-29 09:35:44 -0700

    .

--- app/api/multi-insights/route.ts
+++ app/api/multi-insights/route.ts
@@ -3,73 +3,84 @@ import { getURL } from '@/utils/helpers';
 import { createClient } from '@supabase/supabase-js';
 import { NextResponse } from 'next/server';
 
-export const runtime = 'edge'
-export const maxDuration = 300
+export const runtime = 'edge';
+export const dynamic = 'force-dynamic';
 
-export async function GET(req: Request) {
-  async function queueInsightTask(user: any) {
-    const taskData = {
-      userId: user.id,
-      timezone: user.timezone,
-      fullName: user.full_name,
-      telegramChatId: user.telegram_chat_id
-    };
+export async function GET(request: Request) {
+  const encoder = new TextEncoder();
 
-    const baseUrl = getURL().replace(/\/$/, '')
-    const url = baseUrl + '/api/single-insights';
-    console.log("Queuing task for user:", user, "at url:", url);
-    // const response = await fetch('https://qstash.upstash.io/v1/publish/v1/publish/' + url, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Authorization': 'Bearer ' + process.env.QSTASH_TOKEN!,
-    //     // 'Upstash-Forward-My-Header': 'my-value', // TODO: security
-    //     'Upstash-Retries': '3',
-    //     'Content-type': 'application/json'
-    //   },
-    //   body: JSON.stringify(taskData)
-    // });
+  const customReadable = new ReadableStream({
+    async start(controller) {
+      const supabase = createClient<Database>(
+        process.env.SUPABASE_URL!,
+        process.env.SUPABASE_KEY!
+      )
+      const { error, data: users } = await supabase
+        .from('users')
+        .select('id, timezone, full_name, telegram_chat_id')
+        .gte('telegram_chat_id', '');
 
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json'
-      },
-      body: JSON.stringify(taskData)
-    });
+      if (error) {
+        controller.enqueue(encoder.encode("Error fetching users: " + error.message));
+        controller.close();
+        return;
+      }
 
-    const responseData = await response.json();
-    if (!response.ok) {
-      console.log("Failed to queue task for user:", user, "with response:", responseData);
-      throw new Error(responseData.message || "Failed to queue task");
-    }
+      const filteredUsers = users?.filter((user) => user.timezone && user.telegram_chat_id) || [];
 
-    console.log("Task queued successfully for user:", user, "with response:", responseData);
-  }
+      for (const user of filteredUsers) {
+        try {
+          await queueInsightTask(user);
+          controller.enqueue(encoder.encode(`Task queued successfully for user: ${JSON.stringify(user)}\n`));
+        } catch (error) {
+          controller.enqueue(encoder.encode(`Error queuing task for user: ${JSON.stringify(user)} with error: ${error}\n`));
+        }
+      }
 
-  const supabase = createClient<Database>(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_KEY!
-  )
-  const { error, data: users } = await supabase
-    .from('users')
-    .select('id, timezone, full_name, telegram_chat_id')
-    .gte('telegram_chat_id', '');
+      controller.close();
+    },
+  });
 
-  if (error) {
-    console.log("Error fetching users:", error.message);
-    return NextResponse.json({ message: "Error fetching users" }, { status: 500 });
-  }
-
-  // Assuming you want to process users who have a timezone and telegram_chat_id
-  const filteredUsers = users?.filter((user) => user.timezone && user.telegram_chat_id) || [];
-
-  console.log("Got users:", filteredUsers);
-  await Promise.all(filteredUsers.map(async (user) => await queueInsightTask(user)
-    .catch((error) => {
-      console.log("Error queuing task for user:", user, "with error:", error)
-      // H.captureException(error);
-    })));
-
-  return NextResponse.json({ message: "Tasks queued successfully" }, { status: 200 });
+  return new Response(customReadable, {
+    headers: { 'Content-Type': 'text/plain; charset=utf-8' },
+  });
 }
 
+const queueInsightTask = async (user: any) => {
+  const taskData = {
+    userId: user.id,
+    timezone: user.timezone,
+    fullName: user.full_name,
+    telegramChatId: user.telegram_chat_id
+  };
+
+  const baseUrl = getURL().replace(/\/$/, '')
+  const url = baseUrl + '/api/single-insights';
+  console.log("Queuing task for user:", user, "at url:", url);
+  // const response = await fetch('https://qstash.upstash.io/v1/publish/v1/publish/' + url, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Authorization': 'Bearer ' + process.env.QSTASH_TOKEN!,
+  //     // 'Upstash-Forward-My-Header': 'my-value', // TODO: security
+  //     'Upstash-Retries': '3',
+  //     'Content-type': 'application/json'
+  //   },
+  //   body: JSON.stringify(taskData)
+  // });
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(taskData)
+  });
+
+  const responseData = await response.json();
+  if (!response.ok) {
+    console.log("Failed to queue task for user:", user, "with response:", responseData);
+    throw new Error(responseData.message || "Failed to queue task");
+  }
+
+  console.log("Task queued successfully for user:", user, "with response:", responseData);
+}
