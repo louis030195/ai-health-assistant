f90a658c5fe945b9fb0a706fad93659dbe6b885a
Louis Beaumont <louis.beaumont@gmail.com>
2023-09-01 13:17:14 -0700

    .

---
+++ app/api/single-prompts/route.ts
@@ -0,0 +1,237 @@
+import { sendWhatsAppMessage } from '@/app/whatsapp-server';
+import { baseMediarAI, buildBothDataPrompt, buildDayQuestionBothDataPrompt, buildDayQuestionOnlyNeurosityPrompt, buildDayQuestionOnlyOuraRingPrompt, buildDayQuestionTagsPrompt, buildOnlyNeurosityPrompt, buildOnlyOuraRingPrompt, buildOnlyTagsPrompt, generalMediarAIInstructions, generateGoalPrompt } from '@/lib/utils';
+import { Database } from '@/types_db';
+import { createClient } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { llm } from '@/utils/llm';
+import TelegramBot from 'node-telegram-bot-api';
+import PostHogClient from '@/app/posthog-server';
+
+// export const runtime = 'edge'
+export const maxDuration = 300
+
+// curl -X POST -d '{"userId":"20284713-5cd6-4199-8313-0d883f0711a1","timezone":"America/Los_Angeles","fullName":"Louis","telegramChatId":"5776185278", "phone": "+33648140738", "goal": "I aim to increase my productivity by improving my time management skills and maintaining a healthy work-life balance."}' -H "Content-Type: application/json" http://localhost:3000/api/single-prompts
+
+
+export async function POST(req: Request) {
+  const { userId, timezone, fullName, telegramChatId, phone, goal } = await req.json()
+  try {
+    const supabase = createClient<Database>(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_KEY!
+    )
+
+    if (!userId || !timezone || !telegramChatId) {
+      console.log("Missing userId, timezone, fullName, or telegramChatId:", userId, timezone, fullName, telegramChatId);
+      return NextResponse.json({ message: "Missing userId, timezone, fullName, or telegramChatId" }, { status: 200 });
+    }
+
+    console.log("Got user:", userId, timezone, fullName, telegramChatId);
+
+    const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN!, { polling: false });
+
+    const user = {
+      id: userId,
+      timezone,
+      full_name: fullName,
+      telegram_chat_id: telegramChatId,
+      goal: goal || '',
+    }
+    console.log("Processing user:", user);
+
+    const usersToday = new Date().toLocaleString('en-US', { timeZone: user.timezone })
+    const threeDaysAgo = new Date(new Date().setDate(new Date().getDate() - 3)).toLocaleString('en-US', { timeZone: user.timezone });
+
+    // const yesterday = new Date(new Date().setDate(new Date().getDate() - 1)).toLocaleString('en-US', { timeZone: user.timezone })
+    console.log("Yesterday's date for user:", threeDaysAgo);
+    // const yesterdayFromOneAm = new Date(new Date(yesterday).setHours(1, 0, 0, 0)).toLocaleString('en-US', { timeZone: user.timezone })
+    const threeDaysAgoFromOneAm = new Date(new Date(threeDaysAgo).setHours(1, 0, 0, 0)).toLocaleString('en-US', { timeZone: user.timezone });
+
+    // check if there is already an insight at the today timezone of the user
+    const { data: todaysPrompts } = await supabase
+      .from("prompts")
+      .select()
+      .eq("user_id", user.id)
+      .gte('created_at', usersToday)
+
+    // If an insight has already been sent today, skip to the next user
+    if (todaysPrompts && todaysPrompts.length > 0) {
+      console.log("Prompt already sent today for user:", user);
+      return NextResponse.json({ message: "Prompt already sent today" }, { status: 200 });
+    }
+
+    const { data } = await supabase
+      .from('states')
+      .select()
+      .eq('metadata->>label', 'focus')
+      .eq('user_id', user.id)
+      .gte('created_at', threeDaysAgoFromOneAm)
+      .order('created_at', { ascending: false })
+      .limit(10000)
+    console.log("Retrieved Neurosity data:", data?.length);
+
+    // Group by 300 samples and average the probability
+    const neuros = data
+      // filter out < 0.3 probability
+      ?.filter((item) => item.probability && item.probability! > 0.3)
+      ?.reduce((acc: any, curr, index, array) => {
+        if (index % 300 === 0) {
+          const slice = array.slice(index, index + 300);
+          const avgProbability = slice.reduce((sum, item) => sum + (item.probability || 0), 0) / slice.length;
+          acc.push({ created_at: curr.created_at, probability: avgProbability });
+        }
+        return acc;
+      }, []);
+
+    console.log(threeDaysAgoFromOneAm.split(' ')[0])
+    const { data: ouras } = await supabase
+      .from('states')
+      .select()
+      // format as YYYY-MM-DD instead of dd/mm/yyyy
+      .gte('oura->>day', new Date(threeDaysAgoFromOneAm).toISOString().split('T')[0])
+      .eq('user_id', user.id)
+      .order('oura->>day', { ascending: false })
+      .limit(100)
+    console.log("Retrieved Oura data:", ouras?.length);
+
+    const tags = await getTags(user.id, threeDaysAgoFromOneAm);
+    console.log("Retrieved tags:", tags);
+
+    // if the user has nor tags, neuros, ouras, skip to next user
+
+    if (!tags && (!neuros || neuros.length === 0) && (!ouras || ouras.length === 0)) {
+      console.log("No tags, neuros, or ouras for user:", user);
+      return NextResponse.json({ message: "No tags, neuros, or ouras" }, { status: 200 });
+    }
+    console.log("User has neuros of length:", neuros?.length, "and ouras of length:", ouras?.length);
+    console.log("User has tags of length:", tags?.length);
+    let prompt = ''
+
+    console.log("Generating prompt for user:", user);
+
+    let tagsString = '';
+    tags.forEach((tag) => {
+      tag.created_at = new Date(tag.created_at!).toLocaleString('en-US', { timeZone: user.timezone });
+      tagsString += JSON.stringify(tag);
+    });
+
+    let neurosString = '';
+    neuros.forEach((neuro: any) => {
+      neuro.created_at = new Date(neuro.created_at!).toLocaleString('en-US', { timeZone: user.timezone });
+      neurosString += JSON.stringify(neuro);
+    });
+
+    let ourasString = '';
+    ouras?.forEach((oura) => {
+      oura.created_at = new Date(oura.created_at!).toLocaleString('en-US', { timeZone: user.timezone });
+      ourasString += JSON.stringify(oura);
+    });
+
+
+    if (neuros && neuros.length > 0 && ouras && ouras.length > 0) {
+      prompt = await llm(buildDayQuestionBothDataPrompt(neurosString, ourasString, tagsString, user));
+    } else if (neuros && neuros.length > 0) {
+      prompt = await llm(buildDayQuestionOnlyNeurosityPrompt(neurosString, tagsString, user));
+    } else if (ouras && ouras.length > 0) {
+      prompt = await llm(buildDayQuestionOnlyOuraRingPrompt(ourasString, tagsString, user));
+    } else {
+      prompt = await llm(buildDayQuestionTagsPrompt(tagsString, user));
+    }
+
+    console.log("Generated prompt:", prompt);
+
+    if (!prompt) {
+      console.error("No prompt generated for user:", user);
+      return NextResponse.json({ message: "No prompt generated" }, { status: 200 });
+    }
+
+    // return NextResponse.json({ message: "Success" }, { status: 200 });
+
+    const { data: d2, error: e2 } = await supabase.from('chats').insert({
+      text: prompt,
+      user_id: user.id,
+    });
+    console.log("Inserted chat:", d2, "with error:", e2);
+
+    // const hasWhatsapp = await getFeatureFlag(user.id);
+    if (phone) {
+      console.log("Sending whatsapp message to user:", user);
+      // 1. check when was the last whatsapp message with this user
+
+      const { data: lastWhatsappMessage, error: e4 } = await supabase
+        .from('chats')
+        .select()
+        .eq('user_id', user.id)
+        .eq('channel', 'whatsapp')
+        .gte('created_at', usersToday)
+        .order('created_at', { ascending: false })
+        .limit(1)
+
+      if (e4) {
+        console.log("Error fetching last whatsapp message:", e4.message);
+      } else {
+        console.log("Last whatsapp message:", lastWhatsappMessage);
+
+        // 2. if it was less than 24 hours ago, skip
+
+        // 3. if it was more than 24 hours ago, send the template message
+        const lastMessage = lastWhatsappMessage[0];
+        const lastMessageDate = lastMessage?.created_at ? new Date(lastMessage.created_at!).getTime() : 0;
+        const now = new Date().getTime();
+        const diff = now - lastMessageDate;
+        const hours = Math.floor(diff / 1000 / 60 / 60);
+        console.log("Last whatsapp message was:", hours, "hours ago");
+        if (!lastWhatsappMessage || lastWhatsappMessage.length === 0 || hours > 24) {
+
+          // const template = `👋  Hey! Your health matter a lot to me 🥦💪🧠. How can I become a better health assistant for you?`
+          const template = `👋 Hey! Your health matter a lot to me 🥦💪🧠. How can I become a better health assistant for you?`
+          await sendWhatsAppMessage(phone, template);
+        }
+
+        // 4. send the insight
+        await sendWhatsAppMessage(phone, prompt);
+      }
+
+    }
+    const response = await bot.sendMessage(
+      user.telegram_chat_id!,
+      prompt,
+      { parse_mode: 'Markdown' }
+    )
+    console.log("Message sent to:", user.telegram_chat_id, "with response:", response);
+
+    const { error: e3 } = await supabase.from('prompts').insert({
+      text: prompt,
+      user_id: user.id,
+    });
+
+    console.log("Inserted question:", prompt, "with error:", e3);
+
+    return NextResponse.json({ message: "Success" }, { status: 200 });
+  } catch (error) {
+    console.log("Error:", error, userId, timezone, fullName, telegramChatId);
+    return NextResponse.json({ message: "Error" }, { status: 200 });
+  }
+}
+
+
+
+const getTags = async (userId: string, date: string) => {
+  console.log("Getting tags for user:", userId, "since date:", date);
+  const supabase = createClient<Database>(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_KEY!
+  )
+  const { data, error } = await supabase
+    .from('tags')
+    .select('text, created_at')
+    .eq('user_id', userId)
+    .gt('created_at', date)
+
+  if (error) {
+    console.log("Error fetching tags:", error.message);
+  }
+  return data || [];
+};
+
--- lib/utils.ts
+++ lib/utils.ts
@@ -32,7 +32,7 @@ export const generalMediarAIInstructions = `Here are a few rules:
 - Make your responses slightly more conversational to engage the user without losing the concise nature. For example, use phrases like "Looks like you had a peaceful sleep last night!" instead of just "Good sleep quality recorded."
 - Generate insights that go beyond surface-level information. For instance, if the user reports "feeling sad," try to correlate this with data on sleep quality, physical activity, etc., and offer actionable insights.
 - Include occasional prompts or questions designed to help the user form a habit of using Mediar. For example, "Ready for your morning walk?" or "How's your mood today?" Remember not to be too intrusive.
-- At the end of your insights, ask for user feedback subtly. Example: "Was this insight helpful? 👍/👎.", [any problem?](https://cal.com/louis030195/feedback)`
+- At the end of your insights, ask for user feedback subtly. Example: "Was this insight helpful? 👍/👎."`
 
 
 // - Make sure to dynamically adjust the type and frequency of insights based on the user's interaction level and feedback. If a user often gives a 👎, consider changing the approach.
@@ -86,3 +86,71 @@ Here are the goals of the user: ${generateGoalPrompt(user.goal)}
 Assistant:`;
 }
 
+export const generateGoalPromptForQuestions = (goal: string) => {
+  return `The user's goal is: "${goal}". Please adapt your behavior and questions to help the user achieve this goal.`;
+}
+
+export const baseMediarAIForQuestions = `You are an AI assistant that receive message through Telegram/WhatsApp by users.
+The product is called "Mediar" and is described on the landing page: https://mediar.ai as:
+One health assistant for all your wearables
+Mediar is a health assistant in Telegram/WhatsApp that provides daily insights according to events in your life and wearables data.
+
+People wear devices that record their brain, heart, sleep, and physical activity and send tags to you regarding the things they do, eat, drink,
+or experience during the day or their life, on the go, for example: "just ate an apple", or "just had a fight with my wife", or "im sad", or "so low energy tday..".
+Sometimes they send images and we use AI to understand them and extract the caption from them so it might look weird like, 
+the user sending you a pic of his bike and the tag is "a bike put on the side of the road", but it might mean that he went for a bike ride.
+The goal is to have a better understanding of the users' body and mind, and show them patterns and insights about it in order to help them improve their wellbeing. 
+Your task is to generate a single question that will help the user achieve their goals.`
+
+export const generalMediarAIInstructionsForQuestions = `Here are a few rules: 
+- Your question is very concise and straight to the point.
+- Use plenty of emojis to make your question more engaging.
+- Your response will directly be sent to the user so change your language accordingly.
+- Do not mention 'User' or 'Human' in your response, it's implied'.
+- Your question are written in Markdown format.
+- Your question must make the user think and reflect on their recent behavior.
+- Just say the question, NOTHING ELSE. (example: "How confident are you today on a scale of 1 to 5?" - related to his goal of course)
+- Prioritize accuracy by cross-referencing the tags and wearable data to ensure the question are relevant. If you are not sure about something, it's better to not include it.
+- Make your question slightly more conversational to engage the user without losing the concise nature. For example, use phrases like "How did you sleep last night?" instead of just "Sleep quality?"
+- Generate question that go beyond surface-level information. For instance, if the user reports "feeling sad," try to correlate this with data on sleep quality, physical activity, etc., and ask relevant question.
+- At the end of your question, ask for user feedback subtly. Example: "Was this question helpful? 👍/👎."`
+
+export function buildDayQuestionBothDataPrompt(neuros: string, ouras: string, tags: string, user: any) {
+  const userReference = user.fullName ? ` for ${user.fullName}` : '';
+  return `Human: ${baseMediarAIForQuestions}
+Based on these tags: ${JSON.stringify(tags)}, 
+Neurosity states: ${JSON.stringify(neuros)}, 
+and Ouraring states: ${JSON.stringify(ouras)}, 
+what question should we ask the user${userReference} to help them achieve their goal: ${generateGoalPromptForQuestions(user.goal)}?
+${generalMediarAIInstructionsForQuestions}
+Assistant:`;
+}
+
+export function buildDayQuestionOnlyNeurosityPrompt(neuros: string, tags: string, user: any) {
+  const userReference = user.fullName ? ` for ${user.fullName}` : '';
+  return `Human: ${baseMediarAIForQuestions}
+Based on these tags: ${JSON.stringify(tags)}, 
+and Neurosity states: ${JSON.stringify(neuros)}, 
+what question should we ask the user${userReference} to help them achieve their goal: ${generateGoalPromptForQuestions(user.goal)}?
+${generalMediarAIInstructionsForQuestions}
+Assistant:`;
+}
+
+export function buildDayQuestionOnlyOuraRingPrompt(ouras: string, tags: string, user: any) {
+  const userReference = user.fullName ? ` for ${user.fullName}` : '';
+  return `Human: ${baseMediarAIForQuestions}
+Based on these tags: ${JSON.stringify(tags)}, 
+and Ouraring states: ${JSON.stringify(ouras)}, 
+what question should we ask the user${userReference} to help them achieve their goal: ${generateGoalPromptForQuestions(user.goal)}?
+${generalMediarAIInstructionsForQuestions}
+Assistant:`;
+}
+
+export function buildDayQuestionTagsPrompt(tags: string, user: any) {
+  const userReference = user.fullName ? ` for ${user.fullName}` : '';
+  return `Human: ${baseMediarAIForQuestions}
+Based on these tags: ${JSON.stringify(tags)}, 
+what question should we ask the user${userReference} to help them achieve their goal: ${generateGoalPromptForQuestions(user.goal)}?
+${generalMediarAIInstructionsForQuestions}
+Assistant:`;
+}
