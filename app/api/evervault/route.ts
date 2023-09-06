4abdcfbf90a877db18733efe5a561542a8af9fde
Louis Beaumont <louis.beaumont@gmail.com>
2023-09-06 16:14:20 -0700

    .

--- app/api/evervault/route.ts
+++ app/api/evervault/route.ts
@@ -7,7 +7,9 @@ export async function POST(req: Request) {
   const body = await req.json()
 
   console.log('evervaulty', body)
-  const response = await llm(body.prompt, 3, body.model || "claude-2", body.max_tokens_to_sample || 500)
+  // add \n\nAssistant: to the prompt
+  const prompt = body.prompt + "\n\nAssistant:"
+  const response = await llm(prompt, 3, body.model || "claude-2", body.max_tokens_to_sample || 500)
   return new Response(JSON.stringify({ completion: response }), {
     headers: { 'content-type': 'application/json' },
   })
