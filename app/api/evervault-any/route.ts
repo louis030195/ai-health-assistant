1436ae1f3898382f76dde9eb26263ff75e8cb343
Louis Beaumont <louis.beaumont@gmail.com>
2023-09-09 14:22:56 -0700

    evervault-any

--- app/api/evervault-any/route.ts
+++ app/api/evervault-any/route.ts
@@ -5,8 +5,26 @@ export async function POST(req: Request) {
   const body = await req.json()
 
   console.log('evervaulty', body)
-  return new Response(JSON.stringify({ body }), {
+  return new Response(JSON.stringify({ ...body }), {
     headers: { 'content-type': 'application/json' },
   })
 }
+
+/* 
+user like 
+
+const user = {
+  id: 'qwde-dsds-ewew',
+  timezone: 'America/New_York',
+  full_name: 'Bobby',
+  telegram_chat_id: 123456789,
+  goal: 'lose weight w Alice',
+}
+
+*/
+
+
+
+
+// curl -X POST -d '{"id": "qwde-dsds-ewew","timezone": "America/New_York","full_name": "Bobby","telegram_chat_id": 123456789,"goal": "lose weight w Alice"}' -H "Content-Type: application/json" https://mediar-ai.relay.evervault.com/api/evervault-any
 
