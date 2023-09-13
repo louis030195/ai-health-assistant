2dba6806ec742872ac5ac5cd7e50795268c9e7b9
Louis Beaumont <louis.beaumont@gmail.com>
2023-09-13 11:06:44 -0700

    .

---
+++ ios/MediarAI/Components/PhoneNumber.swift
@@ -0,0 +1,20 @@
+//
+//  PhoneNumber.swift
+//  MediarAI
+//
+//  Created by Louis Beaumont on 12/09/2023.
+//
+
+import SwiftUI
+
+struct PhoneNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PhoneNumber_Previews: PreviewProvider {
+    static var previews: some View {
+        PhoneNumber()
+    }
+}
